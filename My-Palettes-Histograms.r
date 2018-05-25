ece40d7f3f5bc512ec6c497d09d62a40d7b4051f
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2018-05-25 22:30:44 +0800

    Add files via upload

---
+++ My-Palettes-Histograms.r
@@ -0,0 +1,26 @@
+Цвета:
+01=RdGy
+02=RdYlBu
+03=Spectral
+04=RdYlGn
+05=RdBu
+06=PuOr
+07=PRGn
+08=YlGnBu
+09= BuGn
+10=Oranges
+11=Blues
+12=PiYG
+13=BrBG
+14=OrRd
+15=YlGn
+16=Purples
+17=YlOrRd
+18=RdPu
+19=GnBu
+20=YlOrBr
+21=PuBuGn
+22=PuRd
+23=Greens
+24=Reds
+25=BuPu
